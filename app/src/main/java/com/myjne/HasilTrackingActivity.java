4b73102df6e3be5f49b91035b389215ccc51a5f5
Mediansyah Malik <mysmalik0@gmail.com>
2020-02-22 13:46:42 +0700

    github

---
+++ app/src/main/java/com/myjne/HasilTrackingActivity.java
@@ -0,0 +1,14 @@
+package com.myjne;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HasilTrackingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_tracking);
+    }
+}
